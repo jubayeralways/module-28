7c18a5d070b5a55f92b076c2bb89d2095367babc
jubayeralways <mdjubayerhossain7@gmail.com>
2026-03-04 16:17:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+// const result = fetch("https://jsonplaceholder.typicode.com/todos/1");
+// console.log(result);
+fetch("https://jsonplaceholder.typicode.com/todos/1").then((Response) =>
+  console.log(Response),
+);
